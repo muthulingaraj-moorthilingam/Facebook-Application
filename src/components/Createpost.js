b09a337612019e5e1d1585e31ec87140beb5f0cd
muthulingaraj moorthilingam <muthutamizha.mp96@gmail.com>
2020-07-26 00:04:38 +0530

    edit

--- src/components/Createpost.js
+++ src/components/Createpost.js
@@ -8,8 +8,16 @@ class Createpost extends React.Component{
 			img_Aa:"/Images/Post-img/FB-Aa.png",
 			render_Bglist:true,
 			textBg:"#fff",
-			classNameAa:"aa-img"
+			classNameAa:"aa-img",
+			changeBgcolor:null,
+			overFlow:null
 		};
+	}
+	setStyle =() =>{
+		this.setState({
+			overFlow:this.state.changeBgcolor === "#F0F2F5" ? "overFlow" : null 
+			
+		});
 	}
 	closePost =(event) =>{
 		this.props.postOpenblock();
@@ -21,7 +29,20 @@ class Createpost extends React.Component{
 			classNameAa:this.state.classNameAa === "aa-img" ? "left-arrow" : "aa-img"
 		});
 	}
+	changeBgcolor =(img) =>{
+		this.setState({
+			changeBgcolor:img
+		})
+	}
 	render(){
+		const bgImgs =[
+				"#F0F2F5","rgb(198, 0, 255)","/Images/Post-img/FB-Post-Tree","/Images/Post-img/FB-Post-Sa","/Images/Post-img/FB-Post-Heart",
+				"/Images/Post-img/FB-Post-Smile","/Images/Post-img/FB-Post-Fire","/Images/Post-img/FB-Post-Walk","/Images/Post-img/FB-Post-Square"
+		];
+
+		const render =bgImgs.map((img)=>	
+			<Bgimgs src={img} key={img} changeBgcolor={this.changeBgcolor} setStyle={this.setStyle} />
+		);
 		return(
 			<div className="post-create-div" >
 				<div className="post-cr-block">
@@ -56,8 +77,8 @@ class Createpost extends React.Component{
 								<div className="textareaPost">
 									<div className="text-area-post">
 										<div className="bg-text-div">
-											<div className="t-comp-post-cr">
-												<TextatraPost />
+											<div className={"t-comp-post-cr "+this.state.overFlow}>
+												<TextatraPost bg={this.state.changeBgcolor} styleFortext={this.state.styleFortext} />
 												<div className="bg-styleing">
 													<div className="bg-img-set">
 														<div className="aa-default-bg" onClick={this.renderBgimg}>
@@ -69,8 +90,10 @@ class Createpost extends React.Component{
 																<div className="bgsty">
 																	<span className={this.state.classNameAa} style={{backgroundImage:"url("+this.state.img_Aa+")"}} title="Emoji"></span>
 																</div>
+																
 															}
 														</div>
+														{ this.state.render_Bglist === false ?  render : ""}
 													</div>
 													<div className="smile-feel">
 														<div className="smile"></div>
@@ -95,13 +118,45 @@ export default Createpost;
 export class TextatraPost extends React.Component{
 	constructor(props){
 		super(props);
-		this.state={};
+		this.state={
+			bgImg:this.props.bg
+		}
 	}
 	render(){
 		return(
-			<div className="t-p-cr-ty">
+			<div className="t-p-cr-ty" style={{backgroundImage:"url("+this.props.bg+"-bg.jpg)"}}>
 				<textarea className="text-post-cr-type" placeholder="What's on your mind, Muthu?" ></textarea>
 			</div>
 		);	
 	}
 }
+
+export class Bgimgs extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={};
+	}
+	changeBgcolor =() =>{
+		this.props.changeBgcolor(this.props.src);
+		this.props.setStyle();
+	}
+	render(){
+		return(
+			<div className="bg-style-post" onClick={this.changeBgcolor}>
+				{
+					this.props.src === "#F0F2F5"
+							?
+					 <span className="img-p-sty r" style={{backgroundColor:this.props.src}}></span>
+							:
+					this.props.src === "rgb(198, 0, 255)" 
+							?
+					<span className="img-p-sty" style={{backgroundColor:this.props.src}}></span>
+							:
+					 <img src={this.props.src+".jpg"} alt="" className="img-p-sty" />
+
+				}
+			</div>
+		);
+	}
+}
+
